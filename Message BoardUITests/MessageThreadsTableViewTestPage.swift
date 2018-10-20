1a446c4dd59838b22208ff0ba4445e7bc6dfe948
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-20 12:58:41 -0700

    adds first testPage implementation

--- Message BoardUITests/MessageThreadsTableViewTestPage.swift
+++ Message BoardUITests/MessageThreadsTableViewTestPage.swift
@@ -14,17 +14,44 @@ struct MessageThreadsTableViewTestPage: TestPage {
     // Elements
     
     // cell
+    func cellAt(index: Int) -> XCUIElement {
+        return app.tables.children(matching: .cell).element(boundBy: index)
+    }
     // cell title
+    func cellTitle(index: Int) -> XCUIElement {
+        return cellAt(index: index).children(matching: .staticText).element
+    }
     // textfield
+    var messageTextField: XCUIElement {
+        return app.textFields["MessageThreadsTableViewController.TextField"]
+    }
     
     // Actions
     
     // finish typing in textfield
+    @discardableResult func typeTextIn(textField: XCUIElement, with text: String) -> MessageThreadsTableViewTestPage {
+        testCase.expect(exists: textField)
+        textField.tap()
+        textField.typeText(text)
+        return self
+    }
     // tap on cell
+    @discardableResult func tapOnMessageThreadCell(index: Int) -> MessageThreadDetailTableViewTestPage {
+        let cell = cellAt(index: index)
+        testCase.expect(exists: cell)
+        cell.tap()
+        return MessageThreadDetailTableViewTestPage(testCase: testCase)
+    }
     
     // Verifications
     
     // new cell created
+    @discardableResult func verifyCell(is index: Int, withText text: String) -> MessageThreadsTableViewTestPage {
+        let title = cellTitle(index: index)
+        testCase.expect(exists: title)
+        XCTAssertEqual(title.title, text)
+        return self
+    }
     // transition to next VC?
     
 }
